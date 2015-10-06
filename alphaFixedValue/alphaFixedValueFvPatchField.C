5bd88304a6d397e2555562651e81d2611aa6e781
Lianhua Zhu <zhulianhua121@gmail.com>
2015-10-06 11:11:12 +0800

    first commit

---
+++ alphaFixedValue/alphaFixedValueFvPatchField.C
@@ -0,0 +1,166 @@
+/*---------------------------------------------------------------------------*\
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2014 OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+License
+    This file is part of OpenFOAM.
+
+    OpenFOAM is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
+    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+    FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+    for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+
+\*---------------------------------------------------------------------------*/
+
+#include "alphaFixedValueFvPatchField.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+namespace Foam
+{
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+//template<class Type>
+//alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField
+//(
+    //const fvPatch& p,
+    //const DimensionedField<Type, volMesh>& iF,
+    //const scalar s
+//)
+//:
+    //fvPatchField<Type>(p, iF), alpha_(s)
+//{}
+
+
+template<class Type>
+alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField
+(
+    const fvPatch& p,
+    const DimensionedField<Type, volMesh>& iF,
+    const dictionary& dict
+)
+:
+    fvPatchField<Type>(p, iF, dict, true)
+    //alpha_(dict.lookupOrDefault("alpha", 0.0))
+{}
+
+
+template<class Type>
+alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField
+(
+    const alphaFixedValueFvPatchField<Type>& ptf,
+    const fvPatch& p,
+    const DimensionedField<Type, volMesh>& iF,
+    const fvPatchFieldMapper& mapper
+
+)
+:
+    fvPatchField<Type>(ptf, p, iF, mapper)
+{
+    if (&iF && mapper.hasUnmapped())
+    {
+        WarningIn
+        (
+            "alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField\n"
+            "(\n"
+            "    const alphaFixedValueFvPatchField<Type>&,\n"
+            "    const fvPatch&,\n"
+            "    const DimensionedField<Type, volMesh>&,\n"
+            "    const fvPatchFieldMapper&\n"
+            ")\n"
+        )   << "On field " << iF.name() << " patch " << p.name()
+            << " patchField " << this->type()
+            << " : mapper does not map all values." << nl
+            << "    To avoid this warning fully specify the mapping in derived"
+            << " patch fields." << endl;
+    }
+}
+
+
+template<class Type>
+alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField
+(
+    const alphaFixedValueFvPatchField<Type>& ptf
+)
+:
+    fvPatchField<Type>(ptf)
+    //alpha_(ptf.alpha())
+{}
+
+
+//template<class Type>
+//alphaFixedValueFvPatchField<Type>::alphaFixedValueFvPatchField
+//(
+    //const alphaFixedValueFvPatchField<Type>& ptf,
+    //const DimensionedField<Type, volMesh>& iF
+//)
+//:
+    //fvPatchField<Type>(ptf, iF),
+    //alpha_(ptf.alpha())
+//{}
+
+
+// * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
+
+template<class Type>
+tmp<Field<Type> > alphaFixedValueFvPatchField<Type>::valueInternalCoeffs
+(
+    const tmp<scalarField>&
+) const
+{
+    return tmp<Field<Type> >
+    (
+        new Field<Type>(this->size(), pTraits<Type>::zero)
+    );
+}
+
+
+template<class Type>
+tmp<Field<Type> > alphaFixedValueFvPatchField<Type>::valueBoundaryCoeffs
+(
+    const tmp<scalarField>&
+) const
+{
+    return *this;
+}
+
+
+template<class Type>
+tmp<Field<Type> > alphaFixedValueFvPatchField<Type>::gradientInternalCoeffs() const
+{
+    return -pTraits<Type>::one*this->patch().deltaCoeffs();
+}
+
+
+template<class Type>
+tmp<Field<Type> > alphaFixedValueFvPatchField<Type>::gradientBoundaryCoeffs() const
+{
+    return this->patch().deltaCoeffs()*(*this);
+}
+
+
+template<class Type>
+void alphaFixedValueFvPatchField<Type>::write(Ostream& os) const
+{
+    fvPatchField<Type>::write(os);
+    this->writeEntry("value", os);
+}
+
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+} // End namespace Foam
+
+// ************************************************************************* //
